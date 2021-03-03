0b67538b14275c0c9649b8371d0c6f762676717b
Konpoulos <64304981+Konpoulos@users.noreply.github.com>
2021-03-03 13:03:18 +0100

    upload

---
+++ Block 3/Personalisation For Media/PFPM Readings/Erik Code/Week 1/untitled0.py
@@ -0,0 +1,8 @@
+#import libraries
+import pandas as pd
+
+#load the dataset
+
+books_df = pd.read_csv('C:/Users/pc/Desktop/MASTER COURSES/Block 3/Personalisation For Media/week 02/data/books/books.csv' ) 
+
+print(books_df.head(5))
